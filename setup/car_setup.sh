e48b54f941a7b838c173cc7758f355a6762c5faf
Anthony Adams <acadams@Anthonys-MacBook-Pro.local>
2018-04-06 20:58:25 -0400

    Removes very silly line in setup script

--- setup/car_setup.sh
+++ setup/car_setup.sh
@@ -3,8 +3,6 @@
 sudo apt-get update
 sudo apt-get install i2c-tools
 sudo apt-get install python-smbus
-cd ~/
-git clone https://github.com/acadams/Freenove_Three-wheeled_Smart_Car_Kit_for_Raspberry_Pi.git
 sudo apt-get install libv4l-dev
 sudo apt-get install libjpeg8-dev
 sudo apt-get install imagemagick
